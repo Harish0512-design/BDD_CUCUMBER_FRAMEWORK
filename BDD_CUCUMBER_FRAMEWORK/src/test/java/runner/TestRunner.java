b5490e8e7d7717a1af1819fa9534879261471215
Harish <harishsomsole@gmail.com>
2023-03-31 15:26:31 +0530

    cmt

--- BDD_CUCUMBER_FRAMEWORK/src/test/java/runner/TestRunner.java
+++ BDD_CUCUMBER_FRAMEWORK/src/test/java/runner/TestRunner.java
@@ -6,21 +6,9 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-<<<<<<< HEAD
-@CucumberOptions(
-		features = {"src/test/resources/features"},
-		glue = {"steps"},
-		plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-				"json:cucumber.json"},
-		monochrome=true,
-		dryRun = false,
-		tags="@REGRESSION"
-		
-		)
-=======
 @CucumberOptions(features = { "src/test/resources/features" }, glue = { "steps" }, plugin = {
 		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-		"json:target/cucumber.json" }, monochrome = true, dryRun = false,
+		"json:cucumber.json" }, monochrome = true, dryRun = false,
 //		tags="@REGRESSION" 
 //		tags = "@REGRESSION AND @SANITY"
 //		tags = "@REGRESSION OR @SANITY"
@@ -28,7 +16,6 @@ import io.cucumber.junit.CucumberOptions;
 		tags = "@ACCOUNTCREATION"
 
 )
->>>>>>> branch 'master' of https://github.com/Harish0512-design/BDD_CUCUMBER_FRAMEWORK.git
 public class TestRunner {
 
 }
